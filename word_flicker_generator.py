b5465eaac8c5bee7357eea0664b44ecf2065f2bc
hoetaekpro <hoetaek@student.snue.ac.kr>
2018-09-20 01:15:58 +0900

    word and picture is put into the ppt according to the template for word flicker

--- word_flicker_generator.py
+++ word_flicker_generator.py
@@ -1,4 +1,5 @@
 from pptx import Presentation
+from PIL import ImageFont
 import os
 
 class PptWordFlickerMaker():
@@ -10,18 +11,33 @@ class PptWordFlickerMaker():
 
     def make_word_flicker_slide(self, path):
         prs = Presentation('template_for_word_flicker.pptx')
-        for i in range(self.num_slide):
-            card_layout = prs.slide_layouts[0]
-            slide = prs.slides.add_slide(card_layout)
+        for num in self.num_slide:
+            card_layout = prs.slide_layouts[num]
+            for word, pic in zip(self.words, self.pics):
 
-            for j, shape in enumerate(slide.placeholders):
-                pic = shape.insert_picture(self.pics[self.num*i + j])
-                pic.crop_left = 0
-                pic.crop_right = 0
-                pic.crop_bottom = 0
-                pic.crop_top = 0
+                slide = prs.slides.add_slide(card_layout)
+                for shape in slide.shapes:
+                    if str(shape.placeholder_format.type) == 'BODY (2)':
+                        shape.text = word
+                        # font = ImageFont.truetype('times.ttf', 12)
+                        # size = font.getsize('Hello world')
+                        # print(size)
+                        shape.left = (prs.slide_width - shape.width) // 2
+
+                for shape in slide.shapes:
+                    if str(shape.placeholder_format.type) == 'PICTURE (18)':
+                        picture = shape.insert_picture(pic)
+                        picture.crop_left = 0
+                        picture.crop_right = 0
+                        picture.crop_bottom = 0
+                        picture.crop_top = 0
+
 
         file_path = os.path.join(path, '단어깜빡이.pptx')
         file_path = os.path.abspath(file_path)
         prs.save(file_path)
         return file_path
+
+if __name__=='__main__':
+    ppt = PptWordFlickerMaker([['hello', 'python_.png']], [0, 1, 2, 3])
+    ppt.make_word_flicker_slide('.')
