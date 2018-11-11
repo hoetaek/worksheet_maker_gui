15d1dd62bd2321105cd6a3d76f97213bf36df76a
hoetaek@student.snue.ac.kr <htlove1!Github>
2018-11-12 08:12:21 +0900

    words to labels

--- label_generator.py
+++ label_generator.py
@@ -4,9 +4,12 @@ from docx.oxml import OxmlElement
 from docx.oxml.ns import qn
 
 class Picture2Table:
-    def __init__(self, template, pics):
+    def __init__(self, template, words=[], korean=[], pics=[]):
         self.template = Document(template)
+        self.words = words
+        self.korean = korean
         self.pics = pics
+        self.tabel = self.template.tables[0]
         self.get_cell_size()
 
     def get_cell_size(self):
@@ -23,7 +26,6 @@ class Picture2Table:
 
 
     def put_pic_to_cell(self):
-        self.tabel = self.template.tables[0]
         for i, row in enumerate(self.tabel.rows):
             for j, cell in enumerate(row.cells):
                 for paragraph in cell.paragraphs:
@@ -39,10 +41,41 @@ class Picture2Table:
                     tcVAlign = OxmlElement('w:vAlign')
                     tcVAlign.set(qn('w:val'), "center")
                     tcPr.append(tcVAlign)
-        self.template.save('pic_label.hwp')
+
+    def put_word_to_cell(self):
+        for i, row in enumerate(self.tabel.rows):
+            for j, cell in enumerate(row.cells):
+                for paragraph in cell.paragraphs:
+                    #####가운데 정렬!!
+                    paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
+                    index = i * self.width + j
+                    if index < len(self.pics):
+                        paragraph.add_run(self.words[index])
+                    #####상하 방향에서 가운데 정렬
+                    tc = cell._tc
+                    tcPr = tc.get_or_add_tcPr()
+                    tcVAlign = OxmlElement('w:vAlign')
+                    tcVAlign.set(qn('w:val'), "center")
+                    tcPr.append(tcVAlign)
+
+    def put_korean_to_cell(self):
+        for i, row in enumerate(self.tabel.rows):
+            for j, cell in enumerate(row.cells):
+                for paragraph in cell.paragraphs:
+                    #####가운데 정렬!!
+                    paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
+                    index = i * self.width + j
+                    if index < len(self.pics):
+                        paragraph.add_run(self.korean[index])
+                    #####상하 방향에서 가운데 정렬
+                    tc = cell._tc
+                    tcPr = tc.get_or_add_tcPr()
+                    tcVAlign = OxmlElement('w:vAlign')
+                    tcVAlign.set(qn('w:val'), "center")
+                    tcPr.append(tcVAlign)
 
 if __name__=='__main__':
     import os
     pics = [os.path.join('flicker_example', f) for f in os.listdir('flicker_example')]
-    pic2table = Picture2Table('template_24.docx', pics)
+    pic2table = Picture2Table('template_24.docx', pics=pics)
     pic2table.put_pic_to_cell()
